3dbf8b191e0dbca9a7091b8bf33dfae4f34cd24d
szabolcs-cs <36082576+szabolcs-cs@users.noreply.github.com>
2021-10-02 17:24:33 +0100

    gradients using depthwise_conv2d
    - changed feature gradient computation to depthwise convolution
    - typo

--- style-transfer.py
+++ style-transfer.py
@@ -5,23 +5,27 @@ from scipy.optimize import minimize
 
 path_to_vgg19 = "vgg_19.ckpt"  # Download from http://download.tensorflow.org/models/vgg_19_2016_08_28.tar.gz
 
-STYLE_LAYERS = {'vgg_19/conv1/conv1_2/BiasAdd:0': 5e+2, #Name of layer with associated weight
-                'vgg_19/conv2/conv2_2/BiasAdd:0': 5e+2,
-                'vgg_19/conv3/conv3_4/BiasAdd:0': 5e+2,
-                'vgg_19/conv4/conv4_4/BiasAdd:0': 5e+2,
-                'vgg_19/conv5/conv5_4/BiasAdd:0': 5e+2}
-CONTENT_LAYER = 'vgg_19/conv4/conv4_2/BiasAdd:0'
+STYLE_LAYERS = {
+    "vgg_19/conv1/conv1_2/BiasAdd:0": 5e2,  # Name of layer with associated weight
+    "vgg_19/conv2/conv2_2/BiasAdd:0": 5e2,
+    "vgg_19/conv3/conv3_4/BiasAdd:0": 5e2,
+    "vgg_19/conv4/conv4_4/BiasAdd:0": 5e2,
+    "vgg_19/conv5/conv5_4/BiasAdd:0": 5e2,
+}
+CONTENT_LAYER = "vgg_19/conv4/conv4_2/BiasAdd:0"
 
-minfn_args = {"method": "L-BFGS-B", "jac": True, "bounds": [[0, 255]], #options for the minimizer
-              "options": {"factr":0, "ftol": 0,"gtol": 1e-07, "maxcor": 64, "maxiter": 16384, "disp": True}}
+kernel = [[[[0.0, 0, 0], [-1, 0, 1], [0, 0, 0]], [[0, -1, 0], [0, 0, 0], [0, 1, 0]], [[0, 0, 0], [0, 1, 0], [0, 0, 0]]]]
 
+minfn_args = {
+    "method": "L-BFGS-B",
+    "jac": True,
+    "bounds": [[0, 255]],  # options for the minimizer
+    "options": {"factr": 0, "ftol": 0, "gtol": 1e-07, "maxcor": 64, "maxiter": 16, "disp": True},
+}
 
-def getLayer(L): 
-    '''Returns tensor of the specified name from the default graph'''
-    return tf.get_default_graph().get_tensor_by_name(L)
+getLayer = tf.get_default_graph().get_tensor_by_name
 
-
-def transferStyle(content_image, style_image, tv_loss = 10, content_loss = 0.0002):
+def transferStyle(content_image, style_image, tv_loss=10, content_loss=0.0002):
     image_ = tf.placeholder(tf.float32, shape=[None, None, 3])  # placeholder for input image
     with tf.contrib.slim.arg_scope(vgg.vgg_arg_scope()):
         vgg.vgg_19([image_])  # construct VGG19 with the image as input
@@ -30,16 +34,14 @@ def transferStyle(content_image, style_image, tv_loss = 10, content_loss = 0.000
     loss += content_loss * tf.reduce_mean(tf.square(content_in_ - getLayer(CONTENT_LAYER)))  # content loss
     grams_in_, grams_ = {}, {}  # dictionary for imported and exported style features: layer name -> Tensor
     for layer in STYLE_LAYERS:
-        activations = tf.get_default_graph().get_tensor_by_name(layer)#an activation tensor used for tyle loss
-        activationsX = activations[:, 1:, 1:, :] - activations[:, :-1, 1:, :]#X gradient of the feature map (convolution by [-1, 1])
-        activationsY = activations[:, 1:, 1:, :] - activations[:, 1:, :-1, :]#Y gradient of the feature map (convolution by [-1, 1]^T)
-        activations = tf.concat([activations[:, 1:, 1:, :], activationsX, activationsY], 3)#appending gradients to feature map
-        grams_[layer] = tf.einsum('ijkl,ijkm->ilm', activations, activations)  # Gram matrix calculation (gram matrices exported here)
+        T = getLayer(layer)  # an activation tensor used for style loss
+        activations = tf.nn.depthwise_conv2d(T, tf.transpose(kernel * T.shape[3], [2, 3, 0, 1]), [1,1,1,1], 'SAME')
+        grams_[layer] = tf.einsum("ijkl,ijkm->ilm", activations, activations)  # Gram matrix calculation (gram matrices exported here)
         grams_in_[layer] = tf.placeholder(tf.float32, shape=grams_[layer].get_shape())  # gram matrices imported here
         grams_[layer] /= 1e-6 * tf.sqrt(tf.reduce_sum(tf.square(grams_[layer])))  # Normalize gram matrix (multiplication for numerical stability)
         diff = grams_[layer] - grams_in_[layer]  # style difference for this style layer
         loss += STYLE_LAYERS[layer] * tf.log(10000 + tf.reduce_mean(tf.square(diff)))  # accumulate style loss, use log to balance gradients
-    loss = tf.to_double(loss) #loss read from here
+    loss = tf.to_double(loss)  # loss read from here
     image_grad_ = tf.to_double(tf.contrib.layers.flatten(tf.gradients(loss, image_)))  # gradients of the loss read from here
     with tf.Session() as sess:
         tf.train.Saver().restore(sess, path_to_vgg19)  # load VGG parameters from .ckpt file
@@ -51,12 +53,13 @@ def transferStyle(content_image, style_image, tv_loss = 10, content_loss = 0.000
 
         def dfdx(image):  # function passed to L-BFGS that returns the loss and its gradient
             return sess.run([loss, image_grad_], {**style_dict, content_in_: content, image_: np.reshape(image, content_image.shape)})
-        minfn_args["bounds"] *= np.array(content_image).size#set the bounds [0, 255] for each pixel for the optimizer
-        image = minimize(dfdx, np.float64(np.reshape(content_image, -1)), **minfn_args).x # run the minimizer
-        sess.run(tf.write_file("result.png", tf.image.encode_png(tf.reshape(tf.saturate_cast(image, tf.uint8), np.array(content_image).shape))))#write result to disk
+
+        minfn_args["bounds"] *= np.array(content_image).size  # set the bounds [0, 255] for each pixel for the optimizer
+        image = minimize(dfdx, np.float64(np.reshape(content_image, -1)), **minfn_args).x  # run the minimizer
+        sess.run(
+            tf.write_file("result.png", tf.image.encode_png(tf.reshape(tf.saturate_cast(image, tf.uint8), np.array(content_image).shape)))
+        )  # write result to disk
 
 
 if __name__ == "__main__":
     transferStyle(content_image="content_image.png", style_image="style_image.jpg")
-
-
